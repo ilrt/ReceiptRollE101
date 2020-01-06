852bd219f6655fbfcad894d8fd83647fbe278bc6
Mike Jones <mike.a.jones@bristol.ac.uk>
2020-01-06 16:57:09 +0000

    Updated the roll data methods to use the common library

--- receipt_roll/common.py
+++ receipt_roll/common.py
@@ -1,10 +1,11 @@
 # CSV column headers
 
-MEM_COL = 'Membrane'
-TERM_COL = 'Term'
-DAY_COL = 'Day'
-DATE_COL = 'Date'
-VAL_COL = 'Value'
-PENCE_COL = 'Pence'
-SOURCE_COL = 'Source'
-DETAILS_COL = 'Details'
+MEM_COL = 'Membrane'        # The membrane the entry is recorded
+TERM_COL = 'Term'           # The term of the entry (Michaelmas, Hilary, Easter and Trinity)
+DAY_COL = 'Day'             # The day. e.g. 'Saturday'
+DATE_COL = 'Date'           # The date of the entry, e.g. 1302-07-11
+VAL_COL = 'Value'           # Value extracted from the details. e.g. '½ mark'
+PENCE_COL = 'Pence'         # The pence equivalent of the value, easier for comparisons
+SOURCE_COL = 'Source'       # The source (often geographical), e.g. 'Dublin'
+DETAILS_COL = 'Details'     # The details, e.g. 'The same Nicholas, ½ mark for falsely raising hue and cry.'
+PSD_COL = '£.s.d.'          # £.s.d. (computed from the Pence)
--- receipt_roll/roll_data.py
+++ receipt_roll/roll_data.py
@@ -1,32 +1,23 @@
 import pandas as pd
-import receipt_roll.money as money
-
-# Constants (CSV or data frame column headers)
-MEM = 'Membrane'  # The membrane the entry is recorded
-TERM = 'Term'  # The term of the entry (Michaelmas, Hilary, Easter and Trinity)
-DATE = 'Date'  # The date of the entry, e.g. 1302-07-11
-DAY = 'Day'  # The day. e.g. 'Saturday'
-SOURCE = 'Source'  # The source (often geographical), e.g. 'DUBLIN'
-ENTRY = 'Entry'  # Entry details, e.g. 'The same Nicholas, ½ mark for falsely raising hue and cry.'
-VAL = 'Value'  # Value extracted from the details. e.g. '½ mark'
-PENCE = 'Pennies'  # The pence equivalent of the value, easier for comparisons
+import settings
+from receipt_roll import money, common
 
 
 def roll_as_df():
     """ Return the CSV file as a pandas data frame"""
-    return pd.read_csv('roll.csv')
+    return pd.read_csv(settings.ROLL_CSV)
 
 
-def daily_sum_df(df):
-    """ Create a new data frame of daily sums in pence and the equivalent £.s.d. """
+def daily_sum_from_roll_df(df):
+    """ Create a new data frame of daily sums in pence and the equivalent £.s.d. from the roll data """
 
     data = []
-    columns = [DATE, PENCE, '£.s.d.']
+    columns = [common.DATE_COL, common.PENCE_COL, common.PSD_COL]
 
-    date_group = df.groupby(DATE)
+    date_group = df.groupby(common.DATE_COL)
 
     for date, group in date_group:
-        pence = int(group[PENCE].sum())
+        pence = int(group[common.PENCE_COL].sum())
         row = [date, pence, money.pence_to_psd(pence)]
         data.append(row)
 
