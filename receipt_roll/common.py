ad32e8597a38d7779d3a9f08c2511da7a05ff123
Mike Jones <mike.a.jones@bristol.ac.uk>
2020-01-06 16:29:59 +0000

    Moved column names to a common module

---
+++ receipt_roll/common.py
@@ -0,0 +1,10 @@
+# CSV column headers
+
+MEM_COL = 'Membrane'
+TERM_COL = 'Term'
+DAY_COL = 'Day'
+DATE_COL = 'Date'
+VAL_COL = 'Value'
+PENCE_COL = 'Pence'
+SOURCE_COL = 'Source'
+DETAILS_COL = 'Details'
