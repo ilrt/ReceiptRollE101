30169f3287b8ed375f5517f67a6727b8e4c4feb6
Mike Jones <mike.a.jones@bristol.ac.uk>
2020-01-08 15:58:44 +0000

    added more file names

--- settings.py
+++ settings.py
@@ -6,6 +6,14 @@ DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + '/data'
 # the transcript created by Paul Dryburgh and Brendan Smith of the 1301/2 Irish Exchequer receipt roll
 ROLL_TXT = DATA_DIR + '/roll_1301.txt'
 
-#
+# roll data as csv
 ROLL_CSV = DATA_DIR + '/roll_1301.csv'
+
+# daily sums as csv
 DAILY_SUMS_CSV = DATA_DIR + '/daily_sums_1301.csv'
+
+# report comparing daily sums
+DAILY_SUMS_COMPARE_CSV = DATA_DIR + '/daily_sums_compare.csv'
+
+# receipt roll (number of sheets)
+RECEIPT_ROLL_EXCEL = DATA_DIR + '/receipt_roll_1301.xlsx'
